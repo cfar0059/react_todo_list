23d04ffc7740e6ba67bf681080fe51a06b6c9e89
cfar0059 <cfar0059@gmail.com>
2020-10-07 11:34:51 +0200

    Added API to todo list and respectable function to delete and add to list

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
-import { v4 as uuidv4 } from 'uuid';
+import About from "./components/pages/About";
+import axios from "axios";
+import {v4 as uuidv4} from 'uuid';
 
 import './App.css';
 
 class App extends Component {
     state = {
-        todos: [
-            {
-                id: uuidv4(),
-                title: 'Take out the trash',
-                completed: false
-            },
-            {
-                id: uuidv4(),
-                title: 'Dinner with wife',
-                completed: false
-            },
-            {
-                id: uuidv4(),
-                title: 'Meeting with the boss',
-                completed: false
-            }
-        ]
+        todos: []
+    }
+
+    //Runs right after the component mounts
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            .then(res => this.setState({todos: res.data}))
     }
 
     // Toggle  Complete
@@ -41,38 +34,48 @@ class App extends Component {
 
     // Delete Todo
     delTodo = (id) => {
-        this.setState({
-            todos: [...this.state.todos.filter(todo => todo.id !== id)]
-        });
+        axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+            .then(res => this.setState({
+                todos: [...this.state.todos.filter(todo => todo.id !== id)]
+            }));
     }
 
     // Add Todo
     addTodo = (title) => {
-        const newTodo = {
-            id: uuidv4(),
+        axios.post('https://jsonplaceholder.typicode.com/todos', {
             title: title,
             completed: false
-        }
-        this.setState({
-            todos: [...this.state.todos, newTodo]
-        });
+        })
+            .then(res => {
+                res.data.id = uuidv4();
+                this.setState({ todos: [...this.state.todos, res.data] });
+            });
     }
 
     render() {
         return (
-            <div className="App">
-                <div className="container">
-                    <Header/>
-                    <AddTodo
-                        addTodo={this.addTodo}
-                    />
-                    <Todos
-                        todos={this.state.todos}
-                        markComplete={this.markComplete}
-                        delTodo={this.delTodo}
-                    />
+            <Router>
+                <div className="App">
+                    <div className="container">
+                        <Header/>
+                        {/*This Route will load both of these components*/}
+                        {/*Important - add exact on root path*/}
+                        <Route exact path="/" render={props => (
+                            <React.Fragment>
+                                <AddTodo
+                                    addTodo={this.addTodo}
+                                />
+                                <Todos
+                                    todos={this.state.todos}
+                                    markComplete={this.markComplete}
+                                    delTodo={this.delTodo}
+                                />
+                            </React.Fragment>
+                        )}/>
+                        <Route path="/about" component={About}/>
+                    </div>
                 </div>
-            </div>
+            </Router>
         );
     }
 }
