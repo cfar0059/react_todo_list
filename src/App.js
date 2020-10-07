aa3a98b2982584bc88f9a41a677ee7027263622d
cfar0059 <cfar0059@gmail.com>
2020-10-07 09:55:26 +0200

    Added addtodo input form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 import './App.css';
 
@@ -46,7 +48,11 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+                <div className="container">
+                    <Header/>
+                    <AddTodo/>
+                    <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+                </div>
             </div>
         );
     }
